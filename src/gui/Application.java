c06b03a00db757bf29d7c56faba71d45d48b891d
Kathleen Prasatko <88250122+cult4nthropo@users.noreply.github.com>
2022-05-31 16:09:37 +0200

    Revert "."
    This reverts commit e4c5ffa72644a509ee0a744ba70ae7bbb3d92c00.

--- src/gui/Application.java
+++ src/gui/Application.java
@@ -7,6 +7,7 @@ public class Application {
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 
+			@Override
 			public void run() {
 				Gui gui = new Gui();
 				gui.setVisible(true);
