e4c5ffa72644a509ee0a744ba70ae7bbb3d92c00
Kathleen Prasatko <88250122+cult4nthropo@users.noreply.github.com>
2022-05-31 16:09:08 +0200

    .

--- src/gui/Application.java
+++ src/gui/Application.java
@@ -7,7 +7,6 @@ public class Application {
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 
-			@Override
 			public void run() {
 				Gui gui = new Gui();
 				gui.setVisible(true);
